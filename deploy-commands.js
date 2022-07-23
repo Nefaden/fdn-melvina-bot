5c433dc9ad878d5605563efb012e3e157d442194
Yann Durand <yanndurand.contact@gmail.com>
2022-07-23 23:40:42 +0200

    Commit CI Drone

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,15 +3,15 @@ const { REST } = require('@discordjs/rest');
 const { clientId, guildId, token } = require('./config.json');
 
 const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-	new SlashCommandBuilder().setName('fruis').setDescription('React with fruits!'),
+  new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+  new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+  new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+  new SlashCommandBuilder().setName('fruis').setDescription('React with fruits!'),
 ]
-	.map(command => command.toJSON());
+  .map((command) => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+  .then(() => console.log('Successfully registered application commands.'))
+  .catch(console.error);
--- index.js
+++ index.js
@@ -1,49 +1,63 @@
-// Require the necessary discord.js classes
 const { Client, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
-require('dotenv').config()
+require('dotenv').config();
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-	const { commandName } = interaction;
+  const { commandName } = interaction;
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	} else if (commandName === 'user') {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
-	}
+  if (commandName === 'ping') {
+    await interaction.reply('Pong!');
+  } else if (commandName === 'server') {
+    await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+  } else if (commandName === 'user') {
+    await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+  }
 
-	if (commandName === 'fruis') {
-        console.log("ici")
-		const message = await interaction.reply({ content: 'Reacting with fruits!', fetchReply: true });
+  if (commandName === 'fruis') {
+    const message = await interaction.reply({ content: 'Reacting with fruits!', fetchReply: true });
 
-        console.log("interaction : ", interaction)
-		try {
-			await message.react('🍎');
-			await message.react('🍊');
-			await message.react('🍇');
-		} catch (error) {
-			console.error('One of the emojis failed to react:', error);
-		}
-	}
-});
-
-client.on('messageCreate', message => {
-    if(message.author.id === process.env.NEFA_ID) {
-        message.react("<:fdnPEEPODRINK:997142971185115277>")
+    try {
+      await message.react('🍎');
+      await message.react('🍊');
+      await message.react('🍇');
+    } catch (error) {
+      console.error('One of the emojis failed to react:', error);
     }
-})
+  }
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.id === process.env.ESPRIT_ID) {
+    message.react('<:fdnPEEPODRINK:997142971185115277>');
+  }
+  if (message.author.id === process.env.MINIBABYMEL_ID) {
+    message.react('<:fdnPEEPOPENSIVE:997142974074986546>');
+  }
+  if (message.author.id === process.env.PATON_ID) {
+    message.react('<:fdnLUL:986340657780121680>');
+    message.react('<:fdnCHAUVE:986340646111559740>');
+  }
+
+  if (message.author.id === process.env.PUMPKIN_ID) {
+    message.react('🎃');
+  }
+  if (message.author.id === process.env.GOUPIL_ID) {
+    message.react('🦊');
+  }
+  if (message.author.id === process.env.MONO_ID) {
+    message.react('<:ponceDOG:825776345391824896>');
+  }
+});
 
 // Login to Discord with your client's token
 client.login(token);
