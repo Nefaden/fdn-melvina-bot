56081d3127a7c0e2fdc0f06c01b52902c93814bc
Yann Durand <yanndurand.contact@gmail.com>
2022-08-06 23:00:41 +0200

    Restructing project

--- src/commands/handlers/autoReacMessage.js
+++ src/commands/handlers/autoReacMessage.js
@@ -7,10 +7,10 @@ module.exports = (message) => {
   if (message.author.id === process.env.MINIBABYMEL_ID) {
     message.react('<:fdnPEEPOPENSIVE:997142974074986546>');
   }
-  if (message.author.id === process.env.PATON_ID) {
-    message.react('<:fdnLUL:986340657780121680>');
-    message.react('<:fdnCHAUVE:986340646111559740>');
-  }
+  // if (message.author.id === process.env.PATON_ID) {
+  //   message.react('<:fdnLUL:986340657780121680>');
+  //   message.react('<:fdnCHAUVE:986340646111559740>');
+  // }
 
   if (message.author.id === process.env.PUMPKIN_ID) {
     message.react('🎃');
@@ -18,7 +18,7 @@ module.exports = (message) => {
   if (message.author.id === process.env.GOUPIL_ID) {
     message.react('🦊');
   }
-  if (message.author.id === process.env.MONO_ID) {
-    message.react('<:fdnBEBOU:986362070415048765>');
-  }
+  // if (message.author.id === process.env.MONO_ID) {
+  //   message.react('<:fdnBEBOU:986362070415048765>');
+  // }
 };
